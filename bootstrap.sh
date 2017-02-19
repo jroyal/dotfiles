f059c1dabcea6b3e54afbf437fd7cef9b7d4a2af
James Royal <jroyal@cloudflare.com>
2017-02-19 14:43:40 -0600

    Existing stuff from my work mac

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,10 +7,9 @@ git pull origin master;
 function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
-		--exclude ".osx" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
+		--exclude "LICENSE.txt" \
 		-avh --no-perms . ~;
 	source ~/.bash_profile;
 }
